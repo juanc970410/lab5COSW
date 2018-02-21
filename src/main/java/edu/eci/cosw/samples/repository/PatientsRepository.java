e9c087836ca6412d16efa2d95ccd7490d7fd8235
juanc970410 <juan.c.herrerav@hotmail.com>
2018-02-21 18:48:31 -0500

    avance

---
+++ src/main/java/edu/eci/cosw/samples/repository/PatientsRepository.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cosw.samples.repository;
+
+import edu.eci.cosw.jpa.sample.model.Paciente;
+import edu.eci.cosw.jpa.sample.model.PacienteId;
+import edu.eci.cosw.samples.services.ServicesException;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ *
+ * @author 2103021
+ */
+public interface PatientsRepository extends JpaRepository<Paciente, PacienteId>{
+
+    @Query("select p from Paciente p where size(p.consultas) > ?1")
+    public List<Paciente> topPatients(int n) throws ServicesException ;
+}
---
+++ src/main/java/edu/eci/cosw/samples/services/PatientServiceImpl.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cosw.samples.services;
+
+import edu.eci.cosw.jpa.sample.model.Paciente;
+import edu.eci.cosw.jpa.sample.model.PacienteId;
+import edu.eci.cosw.samples.repository.PatientsRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 2103021
+ */
+@Service
+public class PatientServiceImpl implements PatientServices{
+    
+    @Autowired
+    PatientsRepository pr;
+    
+    @Override
+    public Paciente getPatient(int id, String tipoid) throws ServicesException {
+        PacienteId pid = new PacienteId(id, tipoid);
+        Paciente p = pr.getOne(pid);
+        if(p!=null){
+            return p;
+        }else{
+            throw new ServicesException("Paciente inexistente");
+        }
+    }
+
+    @Override
+    public List<Paciente> topPatients(int n) throws ServicesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
